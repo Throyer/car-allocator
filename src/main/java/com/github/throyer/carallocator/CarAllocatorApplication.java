5f6ec277c59b35e46af78811787e381137c1cabe
Renato Henrique <renato.henrique_98@hotmail.com>
2021-12-11 17:32:58 -0300

    Initial commit

---
+++ src/main/java/com/github/throyer/carallocator/CarAllocatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.throyer.carallocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAllocatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarAllocatorApplication.class, args);
+    }
+
+}
